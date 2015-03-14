927d96fc17fe72f42428d83b84de7c34a8bc5c12
arnldlm <arnoldlam1@gmail.com>
2015-03-14 15:56:14 -0700

    Variable x added

---
+++ MyCalculator/CalculatorBrain.swift
@@ -0,0 +1,106 @@
+//
+//  CalculatorBrain.swift
+//  MyCalculator
+//
+//  Created by Arnold Lam on 2015-03-13.
+//  Copyright (c) 2015 Arnold Lam. All rights reserved.
+//
+
+import Foundation
+
+class CalculatorBrain {
+    private enum Op: Printable
+    {
+        case Operand(Double) // stores the operand (number)
+        case unaryOperation(String, Double -> Double) // stores an operation and the function executing that operation
+        case binaryOperation(String, (Double, Double) -> Double)
+        case Variable(String) // stores the variable
+        
+        var description: String { // converting enum values to string
+            get {
+                switch self {
+                case .Operand(let operand):
+                    return "\(operand)"
+                case .unaryOperation(let symbol, _):
+                    return symbol
+                case .binaryOperation(let symbol, _):
+                    return symbol
+                case .Variable(let symbol):
+                    return "\(symbol)"
+                }
+            }
+        }
+    }
+    
+    private var opStack = [Op]()
+    private var knownOps = [String:Op]()
+    
+    var variableValues = [String:Double]() // stores variable name and variable value
+    
+    init() {
+        knownOps["×"] = Op.binaryOperation("×") {$0 * $1}
+        knownOps["÷"] = Op.binaryOperation("÷") {$1 / $0}
+        knownOps["+"] = Op.binaryOperation("+") {$0 + $1}
+        knownOps["-"] = Op.binaryOperation("-") {$1 - $0}
+        knownOps["√"] = Op.unaryOperation("√") {sqrt($0)}
+        knownOps["sin"] = Op.unaryOperation("sin") {sin($0)}
+        knownOps["cos"] = Op.unaryOperation("cos") {cos($0)}
+        knownOps["tan"] = Op.unaryOperation("tan") {tan($0)}
+    }
+    
+    //parameters: an array of type Op
+    private func evaluate(ops: [Op]) -> (result: Double?, remainingOps: [Op]) {
+        if !ops.isEmpty {
+            var remainingOps = ops
+            let op = remainingOps.removeLast() // pops stack and sets to var op
+            switch op {
+                case .Operand(let operand):
+                    return(operand, remainingOps)
+                case .unaryOperation(_, let operation): // case where top of stack is a unary operation
+                    let operandEvaluation = evaluate(remainingOps)
+                    if let operand = operandEvaluation.result {
+                        return (operation(operand), operandEvaluation.remainingOps)
+                    }
+                case .binaryOperation(_, let operation): // case where top of stack is a binary operation
+                    let op1Evaluation = evaluate(remainingOps)
+                    if let operand1 = op1Evaluation.result {
+                        let op2Evaluation = evaluate(op1Evaluation.remainingOps)
+                        if let operand2 = op2Evaluation.result {
+                            return(operation(operand1, operand2), op2Evaluation.remainingOps)
+                        }
+                    }
+                case .Variable(let symbol):
+                    if let variableValue = variableValues["\(symbol)"] {
+                        println("\(variableValue)")
+                        return (variableValue, remainingOps)
+                    }
+            }
+            
+        }
+        return (nil, ops)
+    }
+    
+    func evaluate() -> Double? {
+        let (result, remainder) = evaluate(opStack)
+        println("\(opStack) = \(result) with \(remainder) left over")
+        return result
+    }
+    
+    func pushOperand(operand: Double) -> Double? {
+        opStack.append(Op.Operand(operand))
+        return evaluate()
+    }
+    
+    func performOperation(symbol:String) -> Double? {
+        if let operation = knownOps[symbol] {
+            opStack.append(operation)
+        }
+        return evaluate()
+    }
+    
+    func pushOperand(symbol: String) -> Double? {
+        opStack.append(Op.Variable(symbol))
+        return evaluate()
+    }
+    
+}
---
+++ MyCalculator/ViewController.swift
@@ -0,0 +1,108 @@
+//
+//  ViewController.swift
+//  MyCalculator
+//
+//  Created by Arnold Lam on 2015-03-13.
+//  Copyright (c) 2015 Arnold Lam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var userIsTyping = false
+    var decimalTyped = false
+    var piTyped = false
+    
+    var brain = CalculatorBrain()
+    
+    @IBOutlet weak var display: UILabel!
+    
+    @IBAction func numInput(sender: UIButton) {
+        let digit = sender.currentTitle
+        if userIsTyping {
+            switch digit! {
+                case ".":
+                    if !decimalTyped {
+                        display.text = display.text! + digit!
+                        decimalTyped = true
+                    }
+                default:
+                    display.text = display.text! + digit!
+
+            }
+        }
+        else {
+            switch digit! {
+                case ".":
+                    if !decimalTyped {
+                        display.text = digit
+                    }
+                default:
+                    display.text = digit!
+            }
+
+            userIsTyping = true
+        }
+    }
+    
+    @IBAction func operate(sender: AnyObject) {
+        if userIsTyping {
+            enter()
+        }
+        
+        if let operation = sender.currentTitle! {
+            if let result = brain.performOperation(operation) {
+                displayValue = result
+            }
+            else {
+                displayValue = 0
+            }
+        }
+    }
+    
+    @IBAction func enter() {
+        userIsTyping = false
+        if let result = brain.pushOperand(displayValue) {
+            displayValue = result
+        } else {
+            displayValue = 0
+        }
+    }
+
+    @IBAction func clear() {
+        display.text = "0"
+        userIsTyping = false
+        decimalTyped = false
+        piTyped = false
+        
+        brain = CalculatorBrain()
+    }
+    
+    @IBAction func variable_x() {
+        if brain.variableValues.isEmpty {
+            brain.pushOperand("x")
+            let variableValue = displayValue
+            brain.variableValues["x"] = variableValue
+            
+            display.text = "x = \(variableValue)"
+            userIsTyping = false
+        }
+        else {
+            displayValue = brain.variableValues["x"]!
+        }
+    }
+    
+    var displayValue: Double {
+        get {
+            return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
+        }
+        set {
+            display.text = "\(newValue)"
+            userIsTyping = false
+        }
+    }
+    
+    
+}
+
